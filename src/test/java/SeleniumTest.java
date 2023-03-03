4e384443247769d4f95fe4b5808ea2525ea036a0
Thien Nguyen <ngkhanhthien94@gmail.com>
2023-03-03 21:27:01 +0700

    init selenium project

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement searchField = driver.findElement(By.cssSelector("[name=q]"));
+        searchField.sendKeys("Selenium");
+        driver.findElement(By.cssSelector("[name=btnK]")).click();
+        sleep(3);
+        driver.close();
+    }
+
+    private static void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

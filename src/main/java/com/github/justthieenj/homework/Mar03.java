26e25d12d3064e0069ed41d05b346f1ad0d081d9
Thien Nguyen <ngkhanhthien94@gmail.com>
2023-03-04 10:32:42 +0700

    homework Mar 3rd

---
+++ src/main/java/com/github/justthieenj/homework/Mar03.java
@@ -0,0 +1,18 @@
+package com.github.justthieenj.homework;
+
+public class Mar03 {
+    /*
+    1. Add testng dependency to pom.xml (find it on https://mvnrepository.com/)
+    2. Create a new class SeleniumLocatorTest in src/test/java
+    3. Create a new test method testLocators() with annotations @Test
+    4. Navigate to https://steamcommunity.com/login/home/
+    5. Find elements by using css selector:
+        - account name text box,
+        - password text box,
+        - login button,
+        - remember me checkbox,
+        - qr code image
+    6. And verify that it is displayed (use isDisplayed() method)
+        e.g. Assert.assertTrue(accountName.isDisplayed());
+     */
+}

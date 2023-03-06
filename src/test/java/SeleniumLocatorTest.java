b0ef9f07e5f91c56a9f53ec84af790fbcbe6ab73
Nguyen Cao Hoang Lan <nchoanglan94@gmail.com>
2023-03-06 10:49:45 +0700

    Fix line breaks Change junit to testng

--- src/test/java/SeleniumLocatorTest.java
+++ src/test/java/SeleniumLocatorTest.java
@@ -1,8 +1,8 @@
-import org.junit.Assert;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
